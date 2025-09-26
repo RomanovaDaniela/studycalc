b297d985def1c6578cae3da6705d18ffcce5357a
Romanova Daniela <romanova.daniela.2019@gmail.com>
2025-09-26 19:10:45 +0200

    perf: minor optimization

--- app.py
+++ app.py
@@ -15,3 +15,7 @@ def abs_val(a):
     return abs(a)
 # added at 2025-09-25T22:45:23.566395
 # NOTE: refactor pass at 2025-09-26T15:46:03.789256
+
+def mul(a, b):
+    return a * b
+# added at 2025-09-26T19:10:45.091441
